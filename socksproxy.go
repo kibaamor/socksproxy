e16213dbedb99ab225c0ab854aba3629fea95a75
qinbo <qinbo@digisky.com>
2018-08-03 17:54:18 +0800

    add debug control

--- socksproxy.go
+++ socksproxy.go
@@ -15,6 +15,7 @@ type Config struct {
 	IP      string        `json:"ip"`
 	Port    int           `json:"port"`
 	Timeout time.Duration `json:"timeout"`
+	Debug   bool          `json:"debug"`
 }
 
 func (config *Config) GetAddr() string {
@@ -51,6 +52,7 @@ func main() {
 			IP:      "0.0.0.0",
 			Port:    8080,
 			Timeout: 8,
+			Debug:   true,
 		}
 	} else {
 		var err error
@@ -65,18 +67,19 @@ func main() {
 		log.Fatalf("listen failed: %v\n", err)
 	}
 	log.Println("listen at: " + config.GetAddr())
-	log.Printf("timeout: %s", config.GetTimeout())
+	log.Printf("timeout: %s\n", config.GetTimeout())
+	log.Printf("debug: %v\n", config.Debug)
 
 	for {
 		client, err := listener.Accept()
 		if err != nil {
 			log.Fatalf("accept failed: %v\n", err)
 		}
-		go handleConn(client, config.GetTimeout())
+		go handleConn(client, config.GetTimeout(), config.Debug)
 	}
 }
 
-func handleConn(client net.Conn, timeout time.Duration) {
+func handleConn(client net.Conn, timeout time.Duration, debug bool) {
 	defer client.Close()
 
 	var buf [1024]byte
@@ -104,6 +107,7 @@ func handleConn(client net.Conn, timeout time.Duration) {
 		log.Printf("unsupport command: %d\n", buf[1])
 	}
 
+	var version string
 	var host string
 	var port string
 	switch {
@@ -117,8 +121,10 @@ func handleConn(client net.Conn, timeout time.Duration) {
 		}
 		if i+1 < n { // socks4a
 			host = string(buf[i+1 : n-1])
+			version = "socks4a"
 		} else { // socks4
 			host = net.IPv4(buf[4], buf[5], buf[6], buf[7]).String()
+			version = "socks4"
 		}
 	case n > 5 && buf[0] == 0x5: // socks5
 		switch {
@@ -137,13 +143,16 @@ func handleConn(client net.Conn, timeout time.Duration) {
 			return
 		}
 		port = strconv.Itoa(int(buf[n-2])<<8 | int(buf[n-1]))
+		version = "socks5"
 	default:
 		log.Printf("unsupport version: %d or invalid request\n", buf[0])
 		return
 	}
 
 	addr := net.JoinHostPort(host, port)
-	log.Printf("addr: %s\n", addr)
+	if debug {
+		log.Printf("version: %s, addr: %s\n", version, addr)
+	}
 	server, err := net.DialTimeout("tcp", addr, timeout)
 	if err != nil {
 		log.Printf("dial to [%s] failed: %v\n", addr, err)
